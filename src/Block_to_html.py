392e944ad677401d1e33ca306b48eded5e0e345f
Ayush Gupta <90502624+Predator792002@users.noreply.github.com>
2024-10-03 22:22:24 +0530

    modified Block_to_html.py

--- src/Block_to_html.py
+++ src/Block_to_html.py
@@ -1,5 +1,12 @@
 from split_blocks import markdown_to_block, block_to_block_type
 def markdown_to_html_node(markdown):
     blocks = markdown_to_block(markdown)
+
+    block_nodes = []
     for block in blocks:
         block_type = block_to_block_type(block)
+
+
+
+def text_to_children(text):
+    pass
